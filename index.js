11b2ce00ab07bfc729424231170ce7d3b8c5e435
buyufs <107732743+buyufs@users.noreply.github.com>
2023-07-18 21:09:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,34 @@
-delete require.cache[require.resolve('axios')];
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
 
-// 导入必要的模块和库
-const express = require('express');
-const axios = require('axios');
+const PORT = process.env.PORT || 80
+const HOST = '0.0.0.0'
 
-// 创建 Express 应用
-const app = express();
+// App
+const app = express()
 
-// 定义用于处理订阅事件的路由
-app.post('/subscription', async (req, res) => {
-  // 处理订阅事件逻辑
+app.use(bodyParser.raw())
+app.use(bodyParser.json({}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-  // 假设从请求中获取到了用户信息和订阅时间
-  const { userId, subscriptionTime } = req.body;
+const client = axios.default
 
-  // 等待 1 分钟
-  await delay(60000);
-
-  // 发送服务提醒消息
-  try {
-    const accessToken = await getAccessToken(); // 获取访问令牌
-    const templateId = 'WsdmQ7-3J-zqj6IxG030OgF9cVinrk6xDssGgSuQuns'; // 替换为您的模板 ID
-    const message = {
-      touser: userId,
-      template_id: templateId,
-      data: {
-        // 根据模板要求设置消息内容
-        keyword1: { value: '服务提醒内容' },
-        keyword2: { value: '更多信息' }
-      }
-    };
-
-    // 使用 axios 发送 POST 请求到微信推送服务消息接口
-    await axios.post(`https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=${accessToken}`, message);
-
-    // 发送成功
-    console.log('服务提醒消息发送成功');
-  } catch (error) {
-    // 发送失败
-    console.error('服务提醒消息发送失败:', error.message);
-  }
-
-  // 返回响应
-  res.status(200).end();
+app.all('/', async (req, res) => {
+    const headers = req.headers
+    const weixinAPI = `http://api.weixin.qq.com/cgi-bin/message/custom/send`
+    const payload = {
+        touser: headers['x-wx-openid'],
+        msgtype: 'text',
+        text: {
+            content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
+        }
+    }
+    // dispatch to wx server
+    const result = await client.post(weixinAPI, payload)
+    console.log('received request', req.body, result.data)
+    res.send('success')
 });
 
-// 辅助函数：延迟指定时间
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// 辅助函数：获取访问令牌
-
-// 辅助函数：获取访问令牌
-async function getAccessToken() {
-  const appId = 'wx1e1f16b64387ae7d'; // 替换为您的小程序的 AppID
-  const appSecret = 'f39e51eda08d414b43d22974d4a23e1b'; // 替换为您的小程序的 AppSecret
-
-  try {
-    const response = await axios.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appId}&secret=${appSecret}`);
-    const accessToken = response.data.access_token;
-    return accessToken;
-  } catch (error) {
-    console.error('获取访问令牌失败:', error.message);
-    throw error;
-  }
-}
-
-// 启动 Express 应用
-const port = 3000;
-app.listen(port, () => {
-  console.log(`应用已启动，监听端口 ${port}`);
-});
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
